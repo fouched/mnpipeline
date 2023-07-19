142e34c03223e7364fc5b3251ea22e382a3ad3fb
Fouché du Preez <fouche@unifi.credit>
2023-07-19 09:32:21 +0200

    Added Book Controller & Model

---
+++ src/main/java/za/co/limehouse/controller/BookController.java
@@ -0,0 +1,27 @@
+package za.co.limehouse.controller;
+
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
+import za.co.limehouse.model.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller("/books")
+public class BookController {
+
+    @Get()
+    @Produces(MediaType.APPLICATION_JSON) // this is actually the default so not really needed
+    public List<Book> index() {
+
+        List<Book> books = new ArrayList<>();
+        Book book = new Book();
+        book.setIsbn("12345");
+        book.setTitle("The Book!");
+        books.add(book);
+
+        return books;
+    }
+}
---
+++ src/main/java/za/co/limehouse/model/Book.java
@@ -0,0 +1,9 @@
+package za.co.limehouse.model;
+
+import lombok.Data;
+
+@Data
+public class Book {
+    private String isbn;
+    private String title;
+}
