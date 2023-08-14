dad282fa73cac8be9352d411c34a84dffa916afb
Fouché du Preez <fouche@unifi.credit>
2023-08-14 15:50:42 +0200

    change 10

--- src/main/java/za/co/limehouse/controller/BookController.java
+++ src/main/java/za/co/limehouse/controller/BookController.java
@@ -19,7 +19,7 @@ public class BookController {
         List<Book> books = new ArrayList<>();
         Book book = new Book();
         book.setIsbn("12345");
-        book.setTitle("Fouche se boek!! 9");
+        book.setTitle("Fouche se boek!! 10");
         books.add(book);
 
         return books;
