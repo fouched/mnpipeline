a9c0ab96a5b0b9b7fb0e517baa4efd8a02b6f940
Fouché du Preez <fouche@unifi.credit>
2023-08-23 11:32:10 +0200

    Updated JenkinsfileBranchApp

--- src/main/java/za/co/limehouse/controller/BookController.java
+++ src/main/java/za/co/limehouse/controller/BookController.java
@@ -19,7 +19,7 @@ public class BookController {
         List<Book> books = new ArrayList<>();
         Book book = new Book();
         book.setIsbn("12345");
-        book.setTitle("Fouche se boek!! 18");
+        book.setTitle("Fouche se boek!! 19");
         books.add(book);
 
         return books;
